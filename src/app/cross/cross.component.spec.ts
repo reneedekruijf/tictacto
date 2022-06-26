c172699a75deb16f2d73dc76e885a10e22ceae55
Renee de Kruijf <renee.dekruijf@ikbenmelle-en-co.nl>
2022-06-26 15:35:52 +0200

    cross component

---
+++ src/app/cross/cross.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CrossComponent } from './cross.component';
+
+describe('CrossComponent', () => {
+  let component: CrossComponent;
+  let fixture: ComponentFixture<CrossComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CrossComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CrossComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
